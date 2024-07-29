d710e90fd1cf36e1111b1fee6fb66e82a102ab8d
HerrPhil <phillip.j.wray.esq@gmail.com>
2024-07-29 13:45:31 -0600

    Test the ListNode code in main()

--- ListNode.java
+++ ListNode.java
@@ -2,8 +2,24 @@ public class ListNode<T> {
 
     public static void main(String [] args) {
 
-        System.out.printf("TODO test ListNode%n");
+        // empty test
+        ListNode<String> emptyString = new ListNode<>(null, null);
+        String emptyValue = emptyString.value();
+        ListNode<String> emptyNext = emptyString.next();
+        System.out.printf("empty value is %s%n", emptyValue == null ? "null" : "not null");
+        System.out.printf("empty next is %s%n", emptyNext == null ? "null" : "not null");
 
+        // non-empty test
+        ListNode<Integer> lastNode = new ListNode<>(123, null);
+        ListNode<Integer> firstNode = new ListNode<>(456, lastNode);
+        Integer firstValue = firstNode.value();
+        ListNode<Integer> nextNode = firstNode.next();
+        Integer nextValue = nextNode.value();
+        ListNode<Integer> nextNode2 = nextNode.next();
+        System.out.printf("first value is %d%n", firstValue);
+        System.out.printf("next value is %d%n", nextValue);
+        System.out.printf("next node is %s%n", nextNode2 == null ? "null" : "not null");
+ 
     }
 
     private T value;
